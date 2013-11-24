888bcd5f45ed6c0bad3895d74aedd771a246b7f9
Kirk Shoop <kirk.shoop@microsoft.com>
2013-11-24 14:32:14 -0800

    move Delay to separate header

--- Rx/CPP/src/cpprx/rx-operators.hpp
+++ Rx/CPP/src/cpprx/rx-operators.hpp
@@ -970,6 +970,12 @@ namespace rxcpp
 #include "operators/Take.hpp"
 #include "operators/Skip.hpp"
 
+//////////////////////////////////////////////////////////////////////
+// 
+// time
+
+#include "operators/Delay.hpp"
+
 namespace rxcpp
 {
 
@@ -1004,75 +1010,6 @@ namespace rxcpp
             });
     }
 
-    //////////////////////////////////////////////////////////////////////
-    // 
-    // time
-
-    template <class T>
-    std::shared_ptr<Observable<T>> Delay(
-        const std::shared_ptr<Observable<T>>& source,
-        Scheduler::clock::duration due,
-        Scheduler::shared scheduler)
-    {
-        return CreateObservable<T>(
-            [=](std::shared_ptr<Observer<T>> observer)
-            -> Disposable
-            {
-                auto cancel = std::make_shared<bool>(false);
-
-                ComposableDisposable cd;
-
-                cd.Add(Disposable([=]{ 
-                    *cancel = true; }));
-
-                SerialDisposable sd;
-                auto wsd = cd.Add(sd);
-
-                cd.Add(Subscribe(
-                    source,
-                // on next
-                    [=](const T& element)
-                    {
-                        auto sched_disposable = scheduler->Schedule(
-                            due, 
-                            [=] (Scheduler::shared) -> Disposable { 
-                                if (!*cancel)
-                                    observer->OnNext(element); 
-                                return Disposable::Empty();
-                            }
-                        );
-                        auto ssd = wsd.lock();
-                        if (ssd)
-                        {
-                            *ssd.get() = std::move(sched_disposable);
-                        }
-                    },
-                // on completed
-                    [=]
-                    {
-                        auto sched_disposable = scheduler->Schedule(
-                            due, 
-                            [=](Scheduler::shared) -> Disposable { 
-                                if (!*cancel)
-                                    observer->OnCompleted(); 
-                                return Disposable::Empty();
-                            }
-                        );
-                        auto ssd = wsd.lock();
-                        if (ssd)
-                        {
-                            *ssd.get() = std::move(sched_disposable);
-                        }
-                    },
-                // on error
-                    [=](const std::exception_ptr& error)
-                    {
-                        if (!*cancel)
-                            observer->OnError(error);
-                    }));
-                return cd;
-            });
-    }
 
     template <class T>
     std::shared_ptr<Observable<T>> Throttle(
