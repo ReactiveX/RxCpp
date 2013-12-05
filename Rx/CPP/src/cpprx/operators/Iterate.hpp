cd1318186f7c669df1ff73a97309da430ca98f81
Kirk Shoop <kirk.shoop@microsoft.com>
2013-12-04 23:38:26 -0800

    work around clang compiler bug

--- Rx/CPP/src/cpprx/operators/Iterate.hpp
+++ Rx/CPP/src/cpprx/operators/Iterate.hpp
@@ -28,7 +28,15 @@ namespace rxcpp
         {
             struct State 
             {
-                explicit State(std::shared_ptr<Range> rangeArg) : cancel(false) {
+                explicit State(std::shared_ptr<Range> rangeArg) {
+                    // finally tracked down issue caused by clang compiler bug.
+                    // initializing in the init list only works sometimes.
+                    // a breakpoint here would eventually show that
+                    // cancel was true instead of false.
+                    // initializing in the body instead works every time.
+                    // or using new instead of std::make_shared to create
+                    // State will cause the init list to work every time
+                    this->cancel = false;
                     this->range = std::move(rangeArg);
                     this->r_cursor = begin(*this->range);
                     this->r_end = end(*this->range); 
@@ -45,8 +53,12 @@ namespace rxcpp
             cd.Add(Disposable([=]{
                 state->cancel = true;
             }));
+            
+            SerialDisposable sd;
+            
+            cd.Add(sd);
 
-            cd.Add(scheduler->Schedule(
+            sd.Set(scheduler->Schedule(
                 fix0([=](Scheduler::shared s, std::function<Disposable(Scheduler::shared)> self) -> Disposable
             {
                 if (state->cancel)
@@ -60,7 +72,8 @@ namespace rxcpp
                 {
                     observer->OnNext(*state->r_cursor);
                     ++state->r_cursor; 
-                    return s->Schedule(std::move(self));
+                    sd.Set(s->Schedule(std::move(self)));
+                    return cd;
                 }
                 return Disposable::Empty();             
             })));
