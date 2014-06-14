9ee3732d132a35d8afc1e0951ab70651f754d789
Kirk Shoop <kirk.shoop@microsoft.com>
2014-06-13 23:20:22 -0700

    add missing script

---
+++ projects/scripts/travis-install.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+set -e
+
+#if OS is linux or is not set
+if [ "$TRAVIS_OS_NAME" = linux -o -z "$TRAVIS_OS_NAME" ]; then
+    sudo apt-get update
+    sudo apt-get install -qq libc++
+    sudo apt-get install cmake clang-3.5 libstdc++6
+    sudo apt-get autoremove
+elif [ "$TRAVIS_OS_NAME" = osx ]; then
+    xcode-select --install
+    brew update
+    brew install cmake
+fi
