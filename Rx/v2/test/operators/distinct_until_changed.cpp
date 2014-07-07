84db8981c8da60cd4cbb2add65721abfe1c46921
Valeriy Kopylov <valery.kopylov@akvelon.com>
2014-07-07 17:28:48 +0400

    Add tests for operators
    buffer, distinct_until_changed, scan, switch_on_next

--- Rx/v2/test/operators/distinct_until_changed.cpp
+++ Rx/v2/test/operators/distinct_until_changed.cpp
@@ -1,9 +1,256 @@
 #include "rxcpp/rx.hpp"
-namespace rxu=rxcpp::util;
-namespace rxsc=rxcpp::schedulers;
+namespace rxu = rxcpp::util;
+namespace rxsc = rxcpp::schedulers;
 
 #include "rxcpp/rx-test.hpp"
 #include "catch.hpp"
+
+SCENARIO("distinct_until_changed - never", "[distinct_until_changed][operators]"){
+    GIVEN("a source"){
+        auto sc = rxsc::make_test();
+        auto w = sc.create_worker();
+        const rxsc::test::messages<int> on;
+
+        auto xs = sc.make_hot_observable({
+            on.on_next(150, 1)
+        });
+
+        WHEN("distinct values are taken"){
+
+            auto res = w.start(
+                [xs]() {
+                    return xs.distinct_until_changed();
+                }
+            );
+
+            THEN("the output is empty"){
+                auto required = std::vector<rxsc::test::messages<int>::recorded_type>();
+                auto actual = res.get_observer().messages();
+                REQUIRE(required == actual);
+            }
+
+            THEN("there was 1 subscription/unsubscription to the source"){
+                auto required = rxu::to_vector({
+                    on.subscribe(200, 1000)
+                });
+                auto actual = xs.subscriptions();
+                REQUIRE(required == actual);
+            }
+        }
+    }
+}
+
+SCENARIO("distinct_until_changed - empty", "[distinct_until_changed][operators]"){
+    GIVEN("a source"){
+        auto sc = rxsc::make_test();
+        auto w = sc.create_worker();
+        const rxsc::test::messages<int> on;
+
+        auto xs = sc.make_hot_observable({
+            on.on_next(150, 1),
+            on.on_completed(250)
+        });
+
+        WHEN("distinct values are taken"){
+
+            auto res = w.start(
+                [xs]() {
+                    return xs.distinct_until_changed();
+                }
+            );
+
+            THEN("the output only contains complete message"){
+                auto required = rxu::to_vector({
+                    on.on_completed(250)
+                });
+                auto actual = res.get_observer().messages();
+                REQUIRE(required == actual);
+            }
+
+            THEN("there was 1 subscription/unsubscription to the source"){
+                auto required = rxu::to_vector({
+                    on.subscribe(200, 250)
+                });
+                auto actual = xs.subscriptions();
+                REQUIRE(required == actual);
+            }
+
+        }
+    }
+}
+
+SCENARIO("distinct_until_changed - return", "[distinct_until_changed][operators]"){
+    GIVEN("a source"){
+        auto sc = rxsc::make_test();
+        auto w = sc.create_worker();
+        const rxsc::test::messages<int> on;
+
+        auto xs = sc.make_hot_observable({
+            on.on_next(150, 1),
+            on.on_next(210, 2),
+            on.on_completed(250)
+        });
+
+        WHEN("distinct values are taken"){
+
+            auto res = w.start(
+                [xs]() {
+                    return xs.distinct_until_changed();
+                }
+            );
+
+            THEN("the output only contains distinct items sent while subscribed"){
+                auto required = rxu::to_vector({
+                    on.on_next(210, 2),
+                    on.on_completed(250)
+                });
+                auto actual = res.get_observer().messages();
+                REQUIRE(required == actual);
+            }
+
+            THEN("there was 1 subscription/unsubscription to the source"){
+                auto required = rxu::to_vector({
+                    on.subscribe(200, 250)
+                });
+                auto actual = xs.subscriptions();
+                REQUIRE(required == actual);
+            }
+
+        }
+    }
+}
+
+SCENARIO("distinct_until_changed - throw", "[distinct_until_changed][operators]"){
+    GIVEN("a source"){
+        auto sc = rxsc::make_test();
+        auto w = sc.create_worker();
+        const rxsc::test::messages<int> on;
+
+        std::runtime_error ex("distinct_until_changed on_error from source");
+        
+        auto xs = sc.make_hot_observable({
+            on.on_next(150, 1),
+            on.on_error(250, ex)
+        });
+
+        WHEN("distinct values are taken"){
+
+            auto res = w.start(
+                [xs]() {
+                    return xs.distinct_until_changed();
+                }
+            );
+
+            THEN("the output only contains only error"){
+                auto required = rxu::to_vector({
+                    on.on_error(250, ex)
+                });
+                auto actual = res.get_observer().messages();
+                REQUIRE(required == actual);
+            }
+
+            THEN("there was 1 subscription/unsubscription to the source"){
+                auto required = rxu::to_vector({
+                    on.subscribe(200, 250)
+                });
+                auto actual = xs.subscriptions();
+                REQUIRE(required == actual);
+            }
+
+        }
+    }
+}
+
+SCENARIO("distinct_until_changed - all changes", "[distinct_until_changed][operators]"){
+    GIVEN("a source"){
+        auto sc = rxsc::make_test();
+        auto w = sc.create_worker();
+        const rxsc::test::messages<int> on;
+
+        auto xs = sc.make_hot_observable({
+            on.on_next(150, 1),
+            on.on_next(210, 2),
+            on.on_next(220, 3),
+            on.on_next(230, 4),
+            on.on_next(240, 5),
+            on.on_completed(250)
+        });
+
+        WHEN("distinct values are taken"){
+
+            auto res = w.start(
+                [xs]() {
+                    return xs.distinct_until_changed();
+                }
+            );
+
+            THEN("the output only contains distinct items sent while subscribed"){
+                auto required = rxu::to_vector({
+                    on.on_next(210, 2),
+                    on.on_next(220, 3),
+                    on.on_next(230, 4),
+                    on.on_next(240, 5),
+                    on.on_completed(250)
+                });
+                auto actual = res.get_observer().messages();
+                REQUIRE(required == actual);
+            }
+
+            THEN("there was 1 subscription/unsubscription to the source"){
+                auto required = rxu::to_vector({
+                    on.subscribe(200, 250)
+                });
+                auto actual = xs.subscriptions();
+                REQUIRE(required == actual);
+            }
+
+        }
+    }
+}
+
+SCENARIO("distinct_until_changed - all same", "[distinct_until_changed][operators]"){
+    GIVEN("a source"){
+        auto sc = rxsc::make_test();
+        auto w = sc.create_worker();
+        const rxsc::test::messages<int> on;
+
+        auto xs = sc.make_hot_observable({
+            on.on_next(150, 1),
+            on.on_next(210, 2),
+            on.on_next(220, 2),
+            on.on_next(230, 2),
+            on.on_next(240, 2),
+            on.on_completed(250)
+        });
+
+        WHEN("distinct values are taken"){
+
+            auto res = w.start(
+                [xs]() {
+                    return xs.distinct_until_changed();
+                }
+            );
+
+            THEN("the output only contains distinct items sent while subscribed"){
+                auto required = rxu::to_vector({
+                    on.on_next(210, 2),
+                    on.on_completed(250)
+                });
+                auto actual = res.get_observer().messages();
+                REQUIRE(required == actual);
+            }
+
+            THEN("there was 1 subscription/unsubscription to the source"){
+                auto required = rxu::to_vector({
+                    on.subscribe(200, 250)
+                });
+                auto actual = xs.subscriptions();
+                REQUIRE(required == actual);
+            }
+
+        }
+    }
+}
 
 SCENARIO("distinct_until_changed - some changes", "[distinct_until_changed][operators]"){
     GIVEN("a source"){
--- Rx/v2/test/operators/scan.cpp
+++ Rx/v2/test/operators/scan.cpp
@@ -5,7 +5,188 @@ namespace rxsc=rxcpp::schedulers;
 #include "rxcpp/rx-test.hpp"
 #include "catch.hpp"
 
-SCENARIO("scan some data with seed", "[scan][operators]"){
+SCENARIO("scan: seed, never", "[scan][operators]"){
+    GIVEN("a test hot observable of ints"){
+        auto sc = rxsc::make_test();
+        auto w = sc.create_worker();
+        const rxsc::test::messages<int> on;
+
+        int seed = 1;
+
+        auto xs = sc.make_hot_observable({
+            on.on_next(150, 1),
+        });
+
+        WHEN("mapped to ints that are one larger"){
+
+            auto res = w.start(
+                [&]() {
+                    return xs
+                        .scan(seed, [](int sum, int x) {
+                            return sum + x;
+                        })
+                        // forget type to workaround lambda deduction bug on msvc 2013
+                        .as_dynamic();
+                }
+            );
+
+            THEN("the output is empty"){
+                auto required = std::vector<rxsc::test::messages<int>::recorded_type>();
+                auto actual = res.get_observer().messages();
+                REQUIRE(required == actual);
+            }
+
+            THEN("there was one subscription and one unsubscription"){
+                auto required = rxu::to_vector({
+                    on.subscribe(200, 1000)
+                });
+                auto actual = xs.subscriptions();
+                REQUIRE(required == actual);
+            }
+        }
+    }
+}
+
+SCENARIO("scan: seed, empty", "[scan][operators]"){
+    GIVEN("a test hot observable of ints"){
+        auto sc = rxsc::make_test();
+        auto w = sc.create_worker();
+        const rxsc::test::messages<int> on;
+
+        int seed = 1;
+
+        auto xs = sc.make_hot_observable({
+            on.on_next(150, 1),
+            on.on_completed(250)
+        });
+
+        WHEN("mapped to ints that are one larger"){
+
+            auto res = w.start(
+                [&]() {
+                    return xs
+                        .scan(seed, [](int sum, int x) {
+                            return sum + x;
+                        })
+                        // forget type to workaround lambda deduction bug on msvc 2013
+                        .as_dynamic();
+                }
+            );
+
+            THEN("the output stops on completion"){
+                auto required = rxu::to_vector({
+                    on.on_completed(250)
+                });
+                auto actual = res.get_observer().messages();
+                REQUIRE(required == actual);
+            }
+
+            THEN("there was one subscription and one unsubscription"){
+                auto required = rxu::to_vector({
+                    on.subscribe(200, 250)
+                });
+                auto actual = xs.subscriptions();
+                REQUIRE(required == actual);
+            }
+        }
+    }
+}
+
+SCENARIO("scan: seed, return", "[scan][operators]"){
+    GIVEN("a test hot observable of ints"){
+        auto sc = rxsc::make_test();
+        auto w = sc.create_worker();
+        const rxsc::test::messages<int> on;
+
+        int seed = 1;
+
+        auto xs = sc.make_hot_observable({
+            on.on_next(150, 1),
+            on.on_next(220, 2),
+            on.on_completed(250)
+        });
+
+        WHEN("mapped to ints that are one larger"){
+
+            auto res = w.start(
+                [&]() {
+                    return xs
+                        .scan(seed, [](int sum, int x) {
+                            return sum + x;
+                        })
+                        // forget type to workaround lambda deduction bug on msvc 2013
+                        .as_dynamic();
+                }
+            );
+
+            THEN("the output stops on completion"){
+                auto required = rxu::to_vector({
+                    on.on_next(220, seed + 2),
+                    on.on_completed(250)
+                });
+                auto actual = res.get_observer().messages();
+                REQUIRE(required == actual);
+            }
+
+            THEN("there was one subscription and one unsubscription"){
+                auto required = rxu::to_vector({
+                    on.subscribe(200, 250)
+                });
+                auto actual = xs.subscriptions();
+                REQUIRE(required == actual);
+            }
+        }
+    }
+}
+
+SCENARIO("scan: seed, throw", "[scan][operators]"){
+    GIVEN("a test hot observable of ints"){
+        auto sc = rxsc::make_test();
+        auto w = sc.create_worker();
+        const rxsc::test::messages<int> on;
+
+        int seed = 1;
+
+        std::runtime_error ex("scan on_error from source");
+
+        auto xs = sc.make_hot_observable({
+            on.on_next(150, 1),
+            on.on_error(250, ex)
+        });
+
+        WHEN("mapped to ints that are one larger"){
+
+            auto res = w.start(
+                [&]() {
+                    return xs
+                        .scan(seed, [](int sum, int x) {
+                            return sum + x;
+                        })
+                        // forget type to workaround lambda deduction bug on msvc 2013
+                        .as_dynamic();
+                }
+            );
+
+            THEN("the output stops on error"){
+                auto required = rxu::to_vector({
+                    on.on_error(250, ex)
+                });
+                auto actual = res.get_observer().messages();
+                REQUIRE(required == actual);
+            }
+
+            THEN("there was one subscription and one unsubscription"){
+                auto required = rxu::to_vector({
+                    on.subscribe(200, 250)
+                });
+                auto actual = xs.subscriptions();
+                REQUIRE(required == actual);
+            }
+        }
+    }
+}
+
+SCENARIO("scan: seed, some data", "[scan][operators]"){
     GIVEN("a test hot observable of ints"){
         auto sc = rxsc::make_test();
         auto w = sc.create_worker();
@@ -50,6 +231,62 @@ SCENARIO("scan some data with seed", "[scan][operators]"){
             THEN("there was one subscription and one unsubscription"){
                 auto required = rxu::to_vector({
                     on.subscribe(200, 250)
+                });
+                auto actual = xs.subscriptions();
+                REQUIRE(required == actual);
+            }
+        }
+    }
+}
+
+SCENARIO("scan: seed, accumulator throws", "[scan][operators]"){
+    GIVEN("a test hot observable of ints"){
+        auto sc = rxsc::make_test();
+        auto w = sc.create_worker();
+        const rxsc::test::messages<int> on;
+
+        int seed = 1;
+
+        std::runtime_error ex("scan on_error from source");
+
+        auto xs = sc.make_hot_observable({
+            on.on_next(150, 1),
+            on.on_next(210, 2),
+            on.on_next(220, 3),
+            on.on_next(230, 4),
+            on.on_next(240, 5),
+            on.on_completed(250)
+        });
+
+        WHEN("mapped to ints that are one larger"){
+
+            auto res = w.start(
+                [&]() {
+                    return xs
+                        .scan(seed, [&](int sum, int x) {
+                            if (x == 4) {
+                                throw ex;
+                            }
+                            return sum + x;
+                        })
+                        // forget type to workaround lambda deduction bug on msvc 2013
+                        .as_dynamic();
+                }
+            );
+
+            THEN("the output stops on error"){
+                auto required = rxu::to_vector({
+                    on.on_next(210, seed + 2),
+                    on.on_next(220, seed + 2 + 3),
+                    on.on_error(230, ex)
+                });
+                auto actual = res.get_observer().messages();
+                REQUIRE(required == actual);
+            }
+
+            THEN("there was one subscription and one unsubscription"){
+                auto required = rxu::to_vector({
+                    on.subscribe(200, 230)
                 });
                 auto actual = xs.subscriptions();
                 REQUIRE(required == actual);
