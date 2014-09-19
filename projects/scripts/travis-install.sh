6ec8ca1b5854a95371878de4a0be559c055d1943
Kirk Shoop <kirk.shoop@microsoft.com>
2014-09-19 07:34:49 -0700

    ci script update

--- projects/scripts/travis-install.sh
+++ projects/scripts/travis-install.sh
@@ -8,26 +8,26 @@ if [ "$TRAVIS_OS_NAME" = linux -o -z "$TRAVIS_OS_NAME" ]; then
     sudo add-apt-repository -y 'deb http://llvm.org/apt/precise/ llvm-toolchain-precise main'
     sudo add-apt-repository -y ppa:28msec/utils # Recent cmake
     sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test # gcc-4.8 backport for clang-3.5
-    sudo apt-get clean
-    sudo apt-get update
+    sudo apt-get clean -qq || echo "ignore clean failure"
+    sudo apt-get update -qq || echo "ignore update failure"
 
-    sudo apt-get install -q --fix-missing cmake libssl-dev
+    sudo apt-get install -qq --allow-unauthenticated --force-yes --fix-missing cmake libssl-dev
 
     if [ "$CC" = clang ]; then
-        sudo apt-get install -q --fix-missing clang-3.6
+        sudo apt-get install -qq --allow-unauthenticated --force-yes --fix-missing clang-3.6
         sudo update-alternatives --install /usr/bin/clang clang /usr/bin/clang-3.6 20
         sudo update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-3.6 20
     fi
 
     if [ "$CC" = gcc ]; then
-        sudo apt-get install -q --fix-missing gcc-4.8 g++-4.8
+        sudo apt-get install -qq --allow-unauthenticated --force-yes --fix-missing gcc-4.8 g++-4.8
         sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 20
         sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 20
     fi
 
 elif [ "$TRAVIS_OS_NAME" = osx ]; then
     xcode-select --install
-    brew update
-    brew doctor
-    brew list cmake || brew install cmake
+    brew update  || echo "suppress failures in order to ignore warnings"
+    brew doctor || echo "suppress failures in order to ignore warnings"
+    brew list cmake || brew install cmake || echo "suppress failures in order to ignore warnings"
 fi
