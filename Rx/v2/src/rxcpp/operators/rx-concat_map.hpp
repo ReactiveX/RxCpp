e32cc0c6261622e182ef6f383911a0454dda92cd
Kirk Shoop <kirk.shoop@microsoft.com>
2015-01-31 09:15:42 -0800

    fixes #58 concat_map_factory initialiser list passes arguments in the wrong order

--- Rx/v2/src/rxcpp/operators/rx-concat_map.hpp
+++ Rx/v2/src/rxcpp/operators/rx-concat_map.hpp
@@ -250,8 +250,8 @@ class concat_map_factory
     coordination_type coordination;
 public:
     concat_map_factory(collection_selector_type s, result_selector_type rs, coordination_type sf)
-        : selectorCollection(std::move(rs))
-        , selectorResult(std::move(s))
+        : selectorCollection(std::move(s))
+        , selectorResult(std::move(rs))
         , coordination(std::move(sf))
     {
     }
