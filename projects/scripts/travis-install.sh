41b0d2338987741a5aab28a99adc0cc952e78a2b
Kirk Shoop <kirk.shoop@microsoft.com>
2015-02-24 22:34:53 -0800

    travis ci changes

--- projects/scripts/travis-install.sh
+++ projects/scripts/travis-install.sh
@@ -4,45 +4,37 @@ set -e
 
 #if OS is linux or is not set
 if [ "$TRAVIS_OS_NAME" = linux -o -z "$TRAVIS_OS_NAME" ]; then
-    wget -O - http://llvm.org/apt/llvm-snapshot.gpg.key|sudo apt-key add -
-    sudo add-apt-repository -y 'deb http://llvm.org/apt/trusty/ llvm-toolchain-trusty main'
-    sudo add-apt-repository -y "deb http://us.archive.ubuntu.com/ubuntu/ trusty main universe"
-    sudo add-apt-repository -y ppa:28msec/utils # Recent cmake
-    sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test # gcc-4.8 backport for clang
+#    wget -O - http://llvm.org/apt/llvm-snapshot.gpg.key|sudo apt-key add -
+#    sudo add-apt-repository -y 'deb http://llvm.org/apt/trusty/ llvm-toolchain-trusty main'
+#    sudo add-apt-repository -y "deb http://us.archive.ubuntu.com/ubuntu/ trusty main universe"
+    sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
     sudo apt-get clean -qq || echo "ignore clean failure"
     sudo apt-get update -qq || echo "ignore update failure"
 
-    sudo apt-get install -qq --allow-unauthenticated --force-yes --fix-missing cmake libssl-dev || echo "ignore install failure"
-
-    if [ "$CC" = clang ]; then
-        sudo apt-get install -qq --allow-unauthenticated --force-yes --fix-missing clang-3.6 || echo "ignore install failure"
-
-	sudo update-alternatives --remove-all clang   || echo "ignore remove failure"
-	sudo update-alternatives --remove-all clang++ || echo "ignore remove failure"
-
-	sudo update-alternatives --install /usr/bin/clang   clang   /usr/bin/clang-3.6 20
-	sudo update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-3.6 20
-
-	sudo update-alternatives --config clang
-	sudo update-alternatives --config clang++
-    fi
+    wget http://www.cmake.org/files/v3.1/cmake-3.1.3-Linux-x86_64.sh
+    chmod a+x cmake-3.1.3-Linux-x86_64.sh
+    sudo ./cmake-3.1.3-Linux-x86_64.sh --skip-license --prefix=/usr/local
+    export PATH=/usr/local/bin:$PATH
+    cmake --version
 
     if [ "$CC" = gcc ]; then
         sudo apt-get install -qq --allow-unauthenticated --force-yes --fix-missing gcc-4.8 g++-4.8 || echo "ignore install failure"
 
-	sudo update-alternatives --remove-all gcc || echo "ignore remove failure"
-	sudo update-alternatives --remove-all g++ || echo "ignore remove failure"
+        sudo update-alternatives --remove-all gcc || echo "ignore remove failure"
+        sudo update-alternatives --remove-all g++ || echo "ignore remove failure"
 
-	sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 20
-	sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 20
+        sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 20
+        sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 20
 
-	sudo update-alternatives --config gcc
-	sudo update-alternatives --config g++
+        sudo update-alternatives --config gcc
+        sudo update-alternatives --config g++
+
+        g++ --version
     fi
 
 elif [ "$TRAVIS_OS_NAME" = osx ]; then
     xcode-select --install
-    brew update  || echo "suppress failures in order to ignore warnings"
+    brew update || echo "suppress failures in order to ignore warnings"
     brew doctor || echo "suppress failures in order to ignore warnings"
     brew list cmake || brew install cmake || echo "suppress failures in order to ignore warnings"
 fi
