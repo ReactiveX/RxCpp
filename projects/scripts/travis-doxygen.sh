df58829acfef854510d4604620c77285979d3f71
Valery Kopylov <v-valkop@microsoft.com>
2015-05-06 16:44:43 +0300

    Generate and publish Doxygen docs on Travis

---
+++ projects/scripts/travis-doxygen.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+set -e
+
+#if OS is linux or is not set
+if [ "$TRAVIS_OS_NAME" = linux -o -z "$TRAVIS_OS_NAME" ]; then
+    if [ "$CC" = gcc ]; then
+        doxygen --version
+        dot -V
+
+        cd projects/build
+        make doc -j2
+        cd ../doxygen/html
+        git init
+        git config user.name "KirkShoop"
+        git config user.email "kirkshoop@microsoft.com"
+        git add *
+        git commit -m "doxygen generated site"
+        git push --force "https://${GH_TOKEN}@github.com/Reactive-Extensions/RxCpp.git" master:gh-pages
+    fi
+fi
