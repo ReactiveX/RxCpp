0f3766bdd7a23244a24b95b47dd252e6d54513bf
Valery Kopylov <v-valkop@microsoft.com>
2015-05-07 13:35:58 +0300

    Uncomment the rest of Doxygen generator

--- projects/scripts/travis-doxygen.sh
+++ projects/scripts/travis-doxygen.sh
@@ -10,12 +10,12 @@ if [ "$TRAVIS_OS_NAME" = linux -o -z "$TRAVIS_OS_NAME" ]; then
 
         cd projects/build
         make doc -j2
-#        cd ../doxygen/html
-#        git init
-#        git config user.name "KirkShoop"
-#        git config user.email "kirkshoop@microsoft.com"
-#        git add *
-#        git commit -m "doxygen generated site"
-#        git push --force "https://${GH_TOKEN}@github.com/Reactive-Extensions/RxCpp.git" master:gh-pages
+        cd ../doxygen/html
+        git init
+        git config user.name "KirkShoop"
+        git config user.email "kirkshoop@microsoft.com"
+        git add *
+        git commit -m "doxygen generated site"
+        git push --force "https://${GH_TOKEN}@github.com/Reactive-Extensions/RxCpp.git" master:gh-pages
     fi
 fi
