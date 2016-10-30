051308b0d2260051ac1573e4c282b61e07500232
Grigoriy Chudnov <g.chudnov@gmail.com>
2016-10-30 10:31:44 +0300

    Decouple filter from observable (#264)
    * decouple filter from observable
    
    * fix missing include
    
    * remove filter_factory
    
    * remove ifdef from filter tests

--- Rx/v2/src/rxcpp/operators/rx-filter.hpp
+++ Rx/v2/src/rxcpp/operators/rx-filter.hpp
@@ -2,6 +2,21 @@
 
 #pragma once
 
+/*! \file rx-filter.hpp
+
+    \brief For each item from this observable use Predicate to select which items to emit from the new observable that is returned.
+
+    \tparam Predicate  the type of the filter function
+
+    \param p  the filter function
+
+    \return  Observable that emits only those items emitted by the source observable that the filter evaluates as true.
+
+    \sample
+    \snippet filter.cpp filter sample
+    \snippet output.txt filter sample
+*/
+
 #if !defined(RXCPP_OPERATORS_RX_FILTER_HPP)
 #define RXCPP_OPERATORS_RX_FILTER_HPP
 
@@ -12,6 +27,16 @@ namespace rxcpp {
 namespace operators {
 
 namespace detail {
+
+template<class... AN>
+struct filter_invalid_arguments {};
+
+template<class... AN>
+struct filter_invalid : public rxo::operator_base<filter_invalid_arguments<AN...>> {
+    using type = observable<filter_invalid_arguments<AN...>, filter_invalid<AN...>>;
+};
+template<class... AN>
+using filter_invalid_t = typename filter_invalid<AN...>::type;
 
 template<class T, class Predicate>
 struct filter
@@ -58,7 +83,7 @@ struct filter
             dest.on_completed();
         }
 
-        static subscriber<value_type, observer<value_type, this_type>> make(dest_type d, test_type t) {
+        static subscriber<value_type, observer_type> make(dest_type d, test_type t) {
             return make_subscriber<value_type>(d, this_type(d, std::move(t)));
         }
     };
@@ -70,29 +95,36 @@ struct filter
     }
 };
 
-template<class Predicate>
-class filter_factory
+}
+
+/*! @copydoc rx-filter.hpp
+*/
+template<class... AN>
+auto filter(AN&&... an)
+    ->      operator_factory<filter_tag, AN...> {
+     return operator_factory<filter_tag, AN...>(std::make_tuple(std::forward<AN>(an)...));
+}
+
+}
+
+template<>
+struct member_overload<filter_tag>
 {
-    typedef rxu::decay_t<Predicate> test_type;
-    test_type predicate;
-public:
-    filter_factory(test_type p) : predicate(std::move(p)) {}
-    template<class Observable>
-    auto operator()(Observable&& source)
-        -> decltype(source.template lift<rxu::value_type_t<rxu::decay_t<Observable>>>(filter<rxu::value_type_t<rxu::decay_t<Observable>>, test_type>(predicate))) {
-        return      source.template lift<rxu::value_type_t<rxu::decay_t<Observable>>>(filter<rxu::value_type_t<rxu::decay_t<Observable>>, test_type>(predicate));
+    template<class Observable, class Predicate,
+        class SourceValue = rxu::value_type_t<Observable>,
+        class Filter = rxo::detail::filter<SourceValue, rxu::decay_t<Predicate>>>
+    static auto member(Observable&& o, Predicate&& p)
+        -> decltype(o.template lift<SourceValue>(Filter(std::forward<Predicate>(p)))) {
+        return      o.template lift<SourceValue>(Filter(std::forward<Predicate>(p)));
+    }
+
+    template<class... AN>
+    static operators::detail::filter_invalid_t<AN...> member(const AN&...) {
+        std::terminate();
+        return {};
+        static_assert(sizeof...(AN) == 10000, "filter takes (Predicate)");
     }
 };
-
-}
-
-template<class Predicate>
-auto filter(Predicate&& p)
-    ->      detail::filter_factory<Predicate> {
-    return  detail::filter_factory<Predicate>(std::forward<Predicate>(p));
-}
-
-}
 
 }
 
