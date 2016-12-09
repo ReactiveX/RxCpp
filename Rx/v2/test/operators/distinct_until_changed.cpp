c1e4f3bd3b4f4294839fefe525b9dea83766279b
Grigoriy Chudnov <g.chudnov@gmail.com>
2016-12-09 23:48:45 +0300

    add an optional BinaryPredicate to distinct_until_changed for element comparison

--- Rx/v2/test/operators/distinct_until_changed.cpp
+++ Rx/v2/test/operators/distinct_until_changed.cpp
@@ -361,3 +361,57 @@ SCENARIO("distinct_until_changed - custom type", "[distinct_until_changed][opera
         }
     }
 }
+
+SCENARIO("distinct_until_changed - custom predicate", "[distinct_until_changed][operators]"){
+    GIVEN("a source"){
+        auto sc = rxsc::make_test();
+        auto w = sc.create_worker();
+        const rxsc::test::messages<int> on;
+
+        auto xs = sc.make_hot_observable({
+            on.next(150, 1),
+            on.next(210, 2), //*
+            on.next(215, 3), //*
+            on.next(220, 3),
+            on.next(225, 2), //*
+            on.next(230, 2),
+            on.next(230, 1), //*
+            on.next(240, 2), //*
+            on.completed(250)
+        });
+
+        WHEN("distinct values are taken"){
+
+            auto res = w.start(
+                [xs]() {
+                    return xs
+                            .distinct_until_changed([](int x, int y) { return x == y; })
+                            // forget type to workaround lambda deduction bug on msvc 2013
+                            .as_dynamic();                                    
+                }
+            );
+
+            THEN("the output only contains distinct items sent while subscribed"){
+                auto required = rxu::to_vector({
+                    on.next(210, 2), //*
+                    on.next(215, 3), //*
+                    on.next(225, 2), //*
+                    on.next(230, 1), //*
+                    on.next(240, 2), //*
+                    on.completed(250)
+                });
+                auto actual = res.get_observer().messages();
+                REQUIRE(required == actual);
+            }
+
+            THEN("there was 1 subscription/unsubscription to the source"){
+                auto required = rxu::to_vector({
+                    on.subscribe(200, 250)
+                });
+                auto actual = xs.subscriptions();
+                REQUIRE(required == actual);
+            }
+
+        }
+    }
+}
