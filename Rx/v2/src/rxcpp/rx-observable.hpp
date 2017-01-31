fdee23084b7e07dce1c422049214c918dc54d170
Grigoriy Chudnov <g.chudnov@gmail.com>
2017-02-01 00:23:22 +0300

    doc update

--- Rx/v2/src/rxcpp/rx-observable.hpp
+++ Rx/v2/src/rxcpp/rx-observable.hpp
@@ -642,11 +642,8 @@ public:
         static_assert(is_operator_factory_for<this_type, OperatorFactory>::value, "Function passed for op() must have the signature Result(SourceObservable)");
     }
 
-    ///
-    /// takes any function that will take a subscriber for this observable and produce a subscriber.
-    /// this is intended to allow externally defined operators, that use make_subscriber, to be connected
-    /// into the expression.
-    ///
+    /*! @copydoc rx-lift.hpp
+     */
     template<class ResultType, class Operator>
     auto lift(Operator&& op) const
         ->      observable<rxu::value_type_t<rxo::detail::lift_operator<ResultType, source_operator_type, Operator>>, rxo::detail::lift_operator<ResultType, source_operator_type, Operator>> {
@@ -680,51 +677,8 @@ public:
     }
     /// \endcond
 
-    /*! Subscribe will cause this observable to emit values to the provided subscriber.
-
-        \tparam ArgN  types of the subscriber parameters
-
-        \param an  the parameters for making a subscriber
-
-        \return  A subscription with which the observer can stop receiving items before the observable has finished sending them.
-
-        The arguments of subscribe are forwarded to rxcpp::make_subscriber function. Some possible alternatives are:
-
-        - Pass an already composed rxcpp::subscriber:
-        \snippet subscribe.cpp subscribe by subscriber
-        \snippet output.txt subscribe by subscriber
-
-        - Pass an rxcpp::observer. This allows subscribing the same subscriber to several observables:
-        \snippet subscribe.cpp subscribe by observer
-        \snippet output.txt subscribe by observer
-
-        - Pass an `on_next` handler:
-        \snippet subscribe.cpp subscribe by on_next
-        \snippet output.txt subscribe by on_next
-
-        - Pass `on_next` and `on_error` handlers:
-        \snippet subscribe.cpp subscribe by on_next and on_error
-        \snippet output.txt subscribe by on_next and on_error
-
-        - Pass `on_next` and `on_completed` handlers:
-        \snippet subscribe.cpp subscribe by on_next and on_completed
-        \snippet output.txt subscribe by on_next and on_completed
-
-        - Pass `on_next`, `on_error`, and `on_completed` handlers:
-        \snippet subscribe.cpp subscribe by on_next, on_error, and on_completed
-        \snippet output.txt subscribe by on_next, on_error, and on_completed
-        .
-
-        All the alternatives above also support passing rxcpp::composite_subscription instance. For example:
-        \snippet subscribe.cpp subscribe by subscription, on_next, and on_completed
-        \snippet output.txt subscribe by subscription, on_next, and on_completed
-
-        If neither subscription nor subscriber are provided, then a new subscription is created and returned as a result:
-        \snippet subscribe.cpp subscribe unsubscribe
-        \snippet output.txt subscribe unsubscribe
-
-        For more details, see rxcpp::make_subscriber function description.
-    */
+    /*! @copydoc rx-subscribe.hpp
+     */
     template<class... ArgN>
     auto subscribe(ArgN&&... an) const
         -> composite_subscription {
