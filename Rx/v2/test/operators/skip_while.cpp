6abcca36a904ce6343e97d1837b5dcc5c629fab1
Kodovsky <martin.kodovsky@firma.seznam.cz>
2017-12-12 14:20:03 +0100

    #378 test completion

--- Rx/v2/test/operators/skip_while.cpp
+++ Rx/v2/test/operators/skip_while.cpp
@@ -248,11 +248,13 @@ SCENARIO("skip_while, error after", "[skip_while][operators]"){
     }
 }
 
-SCENARIO("skip_while, error same", "[skip_while][operators]"){
+SCENARIO("skip_while, error before", "[skip_while][operators]"){
     GIVEN("a source"){
         auto sc = rxsc::make_test();
         auto w = sc.create_worker();
         const rxsc::test::messages<int> on;
+
+		std::runtime_error ex("skip_while on_error from source");
 
         auto xs = sc.make_hot_observable({
             on.next(70, 6),
@@ -268,13 +270,13 @@ SCENARIO("skip_while, error same", "[skip_while][operators]"){
             on.next(410, 15),
             on.next(415, 16),
             on.next(460, 72),
+			on.error(500, ex),
             on.next(510, 76),
             on.next(560, 32),
             on.next(570, -100),
             on.next(580, -3),
             on.next(590, 5),
-            on.next(630, 10),
-            on.error(690, std::runtime_error("error in unsubscribed stream"))
+            on.next(630, 10)
         });
 
         WHEN("only one value is taken"){
@@ -282,7 +284,7 @@ SCENARIO("skip_while, error same", "[skip_while][operators]"){
             auto res = w.start(
                 [xs]() {
                     return xs
-                        .skip_while(not_equal_to(10))
+                        .skip_while(not_equal_to(72))
                         // forget type to workaround lambda deduction bug on msvc 2013
                         .as_dynamic();
                 }
@@ -290,23 +292,8 @@ SCENARIO("skip_while, error same", "[skip_while][operators]"){
 
             THEN("the output only contains items sent while subscribed"){
                 auto required = rxu::to_vector({
-                    on.next(210, 9),
-                    on.next(230, 13),
-                    on.next(270, 7),
-                    on.next(280, 1),
-                    on.next(300, -1),
-                    on.next(310, 3),
-                    on.next(340, 8),
-                    on.next(370, 11),
-                    on.next(410, 15),
-                    on.next(415, 16),
-                    on.next(460, 72),
-                    on.next(510, 76),
-                    on.next(560, 32),
-                    on.next(570, -100),
-                    on.next(580, -3),
-                    on.next(590, 5),
-                    on.completed(630)
+					on.next(460, 72),
+					on.error(500, ex)
                 });
                 auto actual = res.get_observer().messages();
                 REQUIRE(required == actual);
@@ -314,70 +301,7 @@ SCENARIO("skip_while, error same", "[skip_while][operators]"){
 
             THEN("there was 1 subscription/unsubscription to the source"){
                 auto required = rxu::to_vector({
-                    on.subscribe(200, 630)
-                });
-                auto actual = xs.subscriptions();
-                REQUIRE(required == actual);
-            }
-
-        }
-    }
-}
-
-SCENARIO("skip_while, error before", "[skip_while][operators]"){
-    GIVEN("a source"){
-        auto sc = rxsc::make_test();
-        auto w = sc.create_worker();
-        const rxsc::test::messages<int> on;
-
-        auto xs = sc.make_hot_observable({
-            on.next(70, 6),
-            on.next(150, 4),
-            on.next(210, 9),
-            on.next(230, 13),
-            on.next(270, 7),
-            on.next(280, 1),
-            on.next(300, -1),
-            on.next(310, 3),
-            on.next(340, 8),
-            on.next(370, 11),
-            on.next(410, 15),
-            on.next(415, 16),
-            on.next(460, 72),
-            on.next(510, 76),
-            on.next(560, 32),
-            on.next(570, -100),
-            on.next(580, -3),
-            on.next(590, 5),
-            on.next(630, 10),
-            on.error(690, std::runtime_error("error in unsubscribed stream"))
-        });
-
-        WHEN("3 values are taken"){
-
-            auto res = w.start(
-                [xs]() {
-                    return xs
-                        .skip_while(not_equal_to(1))
-                        // forget type to workaround lambda deduction bug on msvc 2013
-                        .as_dynamic();
-                }
-            );
-
-            THEN("the output only contains items sent while subscribed"){
-                auto required = rxu::to_vector({
-                    on.next(210, 9),
-                    on.next(230, 13),
-                    on.next(270, 7),
-                    on.completed(280)
-                });
-                auto actual = res.get_observer().messages();
-                REQUIRE(required == actual);
-            }
-
-            THEN("there was 1 subscription/unsubscription to the source"){
-                auto required = rxu::to_vector({
-                    on.subscribe(200, 280)
+                    on.subscribe(200, 500)
                 });
                 auto actual = xs.subscriptions();
                 REQUIRE(required == actual);
@@ -395,7 +319,7 @@ SCENARIO("skip_while, dispose before", "[skip_while][operators]"){
 
         auto xs = sc.make_hot_observable({
             on.next(70, 6),
-            on.next(150, 4),
+            on.next(150, 100),
             on.next(210, 9),
             on.next(230, 13),
             on.next(270, 7),
@@ -428,10 +352,7 @@ SCENARIO("skip_while, dispose before", "[skip_while][operators]"){
             );
 
             THEN("the output only contains items sent while subscribed"){
-                auto required = rxu::to_vector({
-                    on.next(210, 9),
-                    on.next(230, 13)
-                });
+				std::vector<rxcpp::notifications::recorded<std::shared_ptr<rxcpp::notifications::detail::notification_base<int> > > > required;
                 auto actual = res.get_observer().messages();
                 REQUIRE(required == actual);
             }
@@ -456,7 +377,7 @@ SCENARIO("skip_while, dispose after", "[skip_while][operators]"){
 
         auto xs = sc.make_hot_observable({
             on.next(70, 6),
-            on.next(150, 4),
+			on.next(150, 4), //this is skipped due to delayed subscription
             on.next(210, 9),
             on.next(230, 13),
             on.next(270, 7),
@@ -476,7 +397,7 @@ SCENARIO("skip_while, dispose after", "[skip_while][operators]"){
             on.next(630, 10)
         });
 
-        WHEN("3 values are taken"){
+        WHEN("5 values are taken"){
 
             auto res = w.start(
                 [xs]() {
@@ -490,10 +411,11 @@ SCENARIO("skip_while, dispose after", "[skip_while][operators]"){
 
             THEN("the output only contains items sent while subscribed"){
                 auto required = rxu::to_vector({
-                    on.next(210, 9),
-                    on.next(230, 13),
-                    on.next(270, 7),
-                    on.completed(280)
+    				on.next(280, 1),
+					on.next(300, -1),
+					on.next(310, 3),
+					on.next(340, 8),
+					on.next(370, 11),
                 });
                 auto actual = res.get_observer().messages();
                 REQUIRE(required == actual);
@@ -501,7 +423,7 @@ SCENARIO("skip_while, dispose after", "[skip_while][operators]"){
 
             THEN("there was 1 subscription/unsubscription to the source"){
                 auto required = rxu::to_vector({
-                    on.subscribe(200, 280)
+                    on.subscribe(200, 400)
                 });
                 auto actual = xs.subscriptions();
                 REQUIRE(required == actual);
