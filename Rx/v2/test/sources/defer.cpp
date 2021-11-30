449e086e487f1862b7b85b0f24c72bc134f4a790
Aleksey Loginov <victimsnino@gmail.com>
2021-11-30 09:32:43 +0300

    Test defer

--- Rx/v2/test/sources/defer.cpp
+++ Rx/v2/test/sources/defer.cpp
@@ -57,3 +57,61 @@ SCENARIO("defer stops on completion", "[defer][sources]"){
         }
     }
 }
+
+SCENARIO("defer doesn't provide copies", "[defer][sources][copies]")
+{
+    GIVEN("observable and subscriber")
+    {
+        auto          empty_on_next = [](copy_verifier) {};
+        auto          sub           = rx::make_observer<copy_verifier>(empty_on_next);
+        copy_verifier verifier{};
+        auto          obs = rxcpp::observable<>::defer([&verifier]()
+        {
+            return rxcpp::observable<>::create<copy_verifier>([&verifier](rxcpp::subscriber<copy_verifier> sub)
+            {
+                sub.on_next(verifier);
+                sub.on_completed();
+            });;
+        });
+
+        WHEN("subscribe")
+        {
+            obs.subscribe(sub);
+            THEN("no extra copies")
+            {
+                // 1 copy to final lambda
+                REQUIRE(verifier.get_copy_count() == 1);
+                REQUIRE(verifier.get_move_count() == 0);
+            }
+        }
+    }
+}
+
+
+SCENARIO("defer doesn't provide copies for move", "[defer][sources][copies]")
+{
+    GIVEN("observable and subscriber")
+    {
+        auto          empty_on_next = [](copy_verifier) {};
+        auto          sub           = rx::make_observer<copy_verifier>(empty_on_next);
+        copy_verifier verifier{};
+        auto          obs = rxcpp::observable<>::defer([&verifier]()
+        {
+            return rxcpp::observable<>::create<copy_verifier>([&verifier](rxcpp::subscriber<copy_verifier> sub)
+            {
+                sub.on_next(std::move(verifier));
+                sub.on_completed();
+            });;
+        });
+        WHEN("subscribe")
+        {
+            obs.subscribe(sub);
+            THEN("no extra copies")
+            {
+                REQUIRE(verifier.get_copy_count() == 0);
+                //  1 move to final lambda
+                REQUIRE(verifier.get_move_count() == 1);
+            }
+        }
+    }
+}
